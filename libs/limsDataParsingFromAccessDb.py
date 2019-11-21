08a223bb2643e633646b5acb0a72e1b4770321e0
Avik Datta <reach4avik@yahoo.com>
2019-11-21 13:56:31 +0000

    added lib codes

---
+++ libs/limsDataParsingFromAccessDb.py
@@ -0,0 +1,287 @@
+import os,jaydebeapi
+import pandas as pd
+
+def load_data_from_access_db(access_db_path,quotes_xls_path,ucanaccess_jar_path):
+  try:
+    project_data,sample_data,premadelibs_data,quotes_data = \
+      __read_data_from_access_and_xls_file(\
+        access_db_path=access_db_path,
+        quotes_xls_path=quotes_xls_path,
+        ucanaccess_jar_path=ucanaccess_jar_path)
+    return project_data,sample_data,premadelibs_data,quotes_data
+  except Exception as e:
+    raise ValueError('Failed to parse access db, eooro: {0}'.format(e))
+
+def __read_data_from_access_and_xls_file(access_db_path,quotes_xls_path,ucanaccess_jar_path):
+  try:
+    required_ucanaccess_jars = [\
+      'ucanaccess-4.0.4.jar',
+      'lib/commons-lang-2.6.jar',
+      'lib/commons-logging-1.1.3.jar',
+      'lib/hsqldb.jar',
+      'lib/jackcess-2.1.11.jar']
+    ucanaccess_jars = list()
+    for jar in required_ucanaccess_jars:
+      ucanaccess_jars.append( os.path.join(ucanaccess_jar_path,jar))
+    classpath = ":".join(ucanaccess_jars)
+    project_data = list()
+    sample_data = list()
+    premadelibs_data = list()
+    quotes_data = pd.DataFrame()
+    try:
+        cnxn = \
+          jaydebeapi.connect(
+            "net.ucanaccess.jdbc.UcanaccessDriver",
+            f"jdbc:ucanaccess://{access_db_path};newDatabaseVersion=V2010",
+            ["", ""],
+            classpath)
+        crsr = cnxn.cursor()
+        premadelibs_data_columns = [\
+          "ID",
+          "ProjectID",
+          "Container_ID",
+          "Well_Position",
+          "Library_Name",
+          "Library_Type",
+          "Library_Prep_Kit_Manufacturer",
+          "Library_Prep_Kit_Name",
+          "Library_Prep_Kit_Catalogue_No",
+          "Index_1_ID",
+          "Index1_Sequence",
+          "Index1_Length",
+          "Index_2_ID",
+          "Index2_Sequence",
+          "Index_2_Length",
+          "Library_Volume_ul",
+          "Library_Average_Fragment_Size_bp",
+          "Library_Concentration_ng/ul",
+          "nM_library",
+          "Pool_No",
+          "Spike-in_PhiX_PCT",
+          "Read_1_Primer_name",
+          "Read_2_Primer_name",
+          "Index_1_Primer_name",
+          "Index_2_Primer_name",
+          "Volume_ul_Pool",
+          "Pool_Average_Fragment_Size_bp",
+          "Pool_Concentration_ng/ul",
+          "Pool_nM",
+          "Premade",
+          "Status",
+          "Workflow",
+          "ContainerType",
+          "Species",
+          "LIbraryID"]
+        crsr.execute("""
+        select 
+        `ID`,
+        `ProjectID`,
+        `Container_ID`,
+        `Well_Position`,
+        `Library_Name`,
+        `Library_Type`,
+        `Library_Prep_Kit_(Manufacturer)`,
+        `Library_Prep_Kit_(Name)`,
+        `Library_Prep_Kit_(Catalogue_No)`,
+        `Index_1_ID`,
+        `Index1_(Sequence)`,
+        `Index1_(Length)`,
+        `Index_2_(ID)`,
+        `Index2_(Sequence)`,
+        `Index_2_(Length)`,
+        `Library_Volume_(µl)`,
+        `Library_Average_Fragment_Size_(bp)`,
+        `Library_Concentration_(ng/µl)`,
+        `nM_(library)`,
+        `Pool_No`,
+        `Spike-in_PhiX_(%)`,
+        `Read_1_Primer_name`,
+        `Read_2_Primer_name`,
+        `Index_1_Primer_name`,
+        `Index_2_Primer_name`,
+        `Volume_(µl)_(Pool)`,
+        `Pool_Average_Fragment_Size_(bp)`,
+        `Pool_Concentration_(ng/µl)`,
+        `Pool_nM`,
+        `Premade`,
+        `Status`,
+        `Workflow`,
+        `ContainerType`,
+        `Species`,
+        `LIbraryID`
+        from premadelibs2""")
+        for row in crsr.fetchall():
+          premadelibs_data.append(row)
+        premadelibs_data = pd.DataFrame(premadelibs_data,columns=premadelibs_data_columns)
+        sample_columns=[
+          'ID',
+          'ProjectID',
+          'IGFID',
+          'Container ID',
+          'Well position',
+          'Sample number',
+          'Sample Name',
+          'Sample type',
+          'Species',
+          'Sample description',
+          'Volume (ul)',
+          'Diluent',
+          'Concentration (ng/ul)',
+          'Quantification method',
+          'Bionalyzer trace provided',
+          'RIN value',
+          'DNAse treated?',
+          'Pool number',
+          'Workflow',
+          'Status',
+          'Concentration (ng/ul) -internal value',
+          'Trace file',
+          'ContainerType',
+          'SampleQCPass']
+        crsr.execute("""
+          select
+          `ID`,
+          `ProjectID`,
+          `IGFID`,
+          `Container ID`,
+          `Well position`,
+          `Sample number`,
+          `Sample Name`,
+          `Sample type`,
+          `Species`,
+          `Sample description`,
+          `Volume (ul)`,
+          `Diluent`,
+          `Concentration (ng/ul)`,
+          `Quantification method`,
+          `Bionalyzer trace provided`,
+          `RIN value`,
+          `DNAse treated?`,
+          `Pool number`,
+          `Workflow`,
+          `Status`,
+          `Concentration (ng/ul) -internal value`,
+          `Trace file`,
+          `ContainerType`,
+          `SampleQCPass` from samples""")
+        for row in crsr.fetchall():
+          sample_data.append(row)
+        sample_data = pd.DataFrame(sample_data,columns=sample_columns)
+        project_columns = [
+          'ID',
+          'QuoteID',
+          'ProjectID',
+          'DateCreated',
+          'SignedQuoteReceived',
+          'DateSamplesReceived',
+          'SampleReceived',
+          'LegacyProjectID',
+          'Initial_QC',
+          'Number_of_items_for_Initial_QC',
+          'IQC_Unit_price',
+          'Library_Preparation',
+          'Number_of_items_for_Library_Prep',
+          'LP_Unit_price',
+          'Library_QC',
+          'Number_of_items_for_Library_QC',
+          'LQC_Unit_price',
+          'Sequencing_Type',
+          'Number_of_Lanes',
+          'Seq_Unit_price',
+          'Data_analysis',
+          'Number_of_items_for_Data_Analysis',
+          'DA_Unit_price',
+          'Total_Quoted',
+          'Price_Band',
+          'Cost_of_Project',
+          'estimated_surplus',
+          'ProjectSummary',
+          'VAT',
+          'Grant Code',
+          'Issued',
+          'ProjectTag',
+          'Comments',
+          'Status',
+          'Customer Note',
+          'Library QC_summary',
+          'Sequencing_summary',
+          'Data Analysis_summary',
+          'Misc_summary',
+          'Misc1',
+          'Misc1_description',
+          'Misc1_qty',
+          'Misc1_unitprice',
+          'Instrument Access',
+          'InsAcc_description',
+          'InsAcc_qty',
+          'InsAcc_unitprice',
+          'Main_contact',
+          'Main_contact_email']
+        crsr.execute("""
+          select
+          `ID`,
+          `QuoteID`,
+          `ProjectID`,
+          `DateCreated`,
+          `SignedQuoteReceived`,
+          `DateSamplesReceived`,
+          `SampleReceived`,
+          `LegacyProjectID`,
+          `Initial_QC`,
+          `Number_of_items_for_Initial_QC`,
+          `IQC_Unit_price`,
+          `Library_Preparation`,
+          `Number_of_items_for_Library_Prep`,
+          `LP_Unit_price`,
+          `Library_QC`,
+          `Number_of_items_for_Library_QC`,
+          `LQC_Unit_price`,
+          `Sequencing_Type`,
+          `Number_of_Lanes`,
+          `Seq_Unit_price`,
+          `Data_analysis`,
+          `Number_of_items_for_Data_Analysis`,
+          `DA_Unit_price`,
+          `Total_Quoted`,
+          `Price_Band`,
+          `Cost_of_Project`,
+          `estimated_surplus`,
+          `ProjectSummary`,
+          `VAT`,
+          `Grant Code`,
+          `Issued`,
+          `ProjectTag`,
+          `Comments`,
+          `Status`,
+          `Customer Note`,
+          `Library QC_summary`,
+          `Sequencing_summary`,
+          `Data Analysis_summary`,
+          `Misc_summary`,
+          `Misc1`,
+          `Misc1_description`,
+          `Misc1_qty`,
+          `Misc1_unitprice`,
+          `Instrument Access`,
+          `InsAcc_description`,
+          `InsAcc_qty`,
+          `InsAcc_unitprice`,
+          `Main_contact`,
+          `Main_contact_email`
+          from projects""")
+        for row in crsr.fetchall():
+          project_data.append(row)
+        project_data = pd.DataFrame(project_data,columns=project_columns)
+        project_data['DateCreated'] = \
+          pd.to_datetime(project_data['DateCreated'].fillna(''),format="%Y-%m-%d %H:%M:%S")
+
+        crsr.close()
+        cnxn.close()
+    except Exception as e:
+      crsr.close()
+      cnxn.close()
+      raise ValueError('Failed to fetch data from access db: {0}'.fromat(e))
+    quotes_data = pd.read_excel(quotes_xls_path)
+    return project_data,sample_data,premadelibs_data,quotes_data
+  except Exception as e:
+    raise ValueError('Failed to fetch metadata from access and xls files: {0}'.format(e))
---
+++ libs/processMetadata.py
@@ -0,0 +1,192 @@
+from dbTableSchema import get_table_schema
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+
+def __get_metadata_df(spark):
+  try: 
+    all_new_project_metadata = \
+    spark.sql("""
+      select
+      concat(project.QuoteID,'_', project.ProjectTag) as project_igf_id,
+      project.Library_Preparation,
+      sample.Species,
+      sample.`Sample Name`,
+      sample.`Sample type`,
+      sample.`Sample description`,
+      library.Library_Name,
+      library.Library_Type,
+      quotes.QuoteID,
+      quotes.Main_contact,
+      quotes.Main_contact_email,
+      quotes.Sequencing_Type,
+      quotes.Number_of_Lanes
+      from
+      (
+        select 
+        table3.QuoteID
+        from 
+        (
+          select
+          project.QuoteID,
+          project.ProjectTag,
+          project.DateCreated,
+          sample.IGFID,
+          table2.project_qid,
+          table2.project_igf_id
+          from
+          project
+          left join
+          sample
+          on project.ID = sample.ProjectID
+          left outer join
+          (
+            select
+            registered_projects.project_igf_id,
+            split(replace(registered_projects.project_igf_id,'-','_'),"_")[0] as project_qid
+            from
+            registered_projects
+          )
+          as table2
+          on
+          project.QuoteID==table2.project_qid
+          where
+          table2.project_igf_id is null
+          and
+          project.DateCreated > '2019'
+          order by project.DateCreated desc
+        )
+        as table3
+        group by table3.QuoteID
+        having count(distinct table3.IGFID) > 0
+      )
+      as table4
+      join project on project.QuoteID=table4.QuoteID
+      join sample on project.ID = sample.ProjectID
+      join library on project.ID=library.ProjectID and sample.`Sample Name`=library.`Library_Name`
+      join quotes on project.QuoteID = quotes.QuoteID 
+    """)
+    return all_new_project_metadata
+  except Exception as e:
+    raise ValueError('Failed to parse tables for required metadata, error: {0}'.format(e))
+
+def __get_samplesheet_df(spark):
+  try:
+    all_new_project_samplesheet = \
+    spark.sql("""
+      select
+      sample.IGFID,
+      library.Library_Name,
+      library.Index_1_ID,
+      library.Index1_Sequence,
+      library.Index_2_ID,
+      library.Index2_Sequence,
+      concat(project.QuoteID,'_', project.ProjectTag) as project_igf_id,
+      library.Pool_no
+      from
+      (
+        select 
+        table3.QuoteID
+        from 
+        (
+          select
+          project.QuoteID,
+          project.ProjectTag,
+          project.DateCreated,
+          sample.IGFID,
+          table2.project_qid,
+          table2.project_igf_id
+          from
+          project
+          left join
+          sample
+          on project.ID = sample.ProjectID
+          left outer join
+          (
+            select
+            registered_projects.project_igf_id,
+            split(replace(registered_projects.project_igf_id,'-','_'),"_")[0] as project_qid
+            from
+            registered_projects
+          )
+          as table2
+          on
+          project.QuoteID==table2.project_qid
+          where
+          table2.project_igf_id is null
+          and
+          project.DateCreated > '2019'
+          order by project.DateCreated desc
+        )
+        as table3
+        group by table3.QuoteID
+        having count(distinct table3.IGFID) > 0
+      )
+      as table4
+      join project on project.QuoteID=table4.QuoteID
+      join sample on project.ID = sample.ProjectID
+      join library on project.ID=library.ProjectID and sample.`Sample Name`=library.`Library_Name`
+      order by library.Pool_no
+    """)
+    return all_new_project_samplesheet
+  except Exception as e:
+    raise ValueError('Failed to parse tables for required samplesheet data, error: {0}'.format(e))
+
+def generate_metadata_and_samplesheet(spark,project_data,sample_data,premadelibs_data,quotes_data,registered_projects_csv,output_dir):
+  try:
+    project_schema,sample_schema,library_schema,quotes_schema = \
+      get_table_schema()
+    OUT_DIR_BC = spark.sparkContext.broadcast(output_dir)
+    project_df = spark.createDataFrame(project_data,schema=project_schema)
+    sample_df = spark.createDataFrame(sample_data,schema=sample_schema)
+    library_df = spark.createDataFrame(premadelibs_data,schema=library_schema)
+    quotes_df = spark.createDataFrame(quotes_data,schema=quotes_schema)
+    project_df.createOrReplaceTempView('project')
+    sample_df.createOrReplaceTempView('sample')
+    library_df.createOrReplaceTempView('library')
+    quotes_df.createOrReplaceTempView('quotes')
+    registered_projects = \
+      spark.\
+      read.\
+      format('csv').\
+      option('header','true').\
+      option('path','sql.csv').\
+      option('sep','\t').\
+      load()
+    registered_projects.createOrReplaceTempView('registered_projects')
+    all_new_project_metadata = __get_metadata_df(spark=spark)
+
+    ## Pandas udf for metadata files
+    @pandas_udf(all_new_project_metadata.schema, PandasUDFType.GROUPED_MAP)
+    def __print_grp(pdf):
+      try:
+        if not isinstance(pdf,pd.DataFrame):
+          raise TypeError('Expecting a Pandas df, got: {0}'.format(type(pdf)))
+        out_dir = OUT_DIR_BC.value
+        qid = pdf.drop_duplicates().get('project_igf_id').values[0]
+        csv_output = os.path.join(out_dir,"{0}.csv".format(qid))
+        pdf.drop_duplicates().to_csv(csv_output,index=False)
+        return pdf
+      except Exception as e:
+        raise ValueError(e)
+
+    metadata_records_count = \
+      all_new_project_metadata.groupby('project_igf_id').apply(__print_grp).count()
+    all_new_project_samplesheet = __get_samplesheet_df(spark=spark)
+
+    ## Pandas udf for samplesheet files
+    @pandas_udf(all_new_project_samplesheet.schema, PandasUDFType.GROUPED_MAP)
+    def __print_samplesheet_grp(pdf):
+      try:
+        if not isinstance(pdf,pd.DataFrame):
+          raise TypeError('Expecting a Pandas df, got: {0}'.format(type(pdf)))
+        out_dir = OUT_DIR_BC.value
+        qid = pdf.drop_duplicates().get('project_igf_id').values[0]
+        csv_output = os.path.join(out_dir,"{0}_SampleSheet.csv".format(qid))
+        pdf.drop_duplicates().to_csv(csv_output,index=False)
+        return pdf
+      except Exception as e:
+        raise ValueError(e)
+
+    samplesheet_records_count = \
+      all_new_project_samplesheet.groupby('project_igf_id').apply(__print_samplesheet_grp).count()
+  except Exception as e:
+    raise ValueError('Failed to create new metadata and samplesheet files, error: {0}'.format(e))
